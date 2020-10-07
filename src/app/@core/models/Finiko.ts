0e3f6e3b1391de47750b52bbaa5fb6698c03781d
gerasimov <gerasimov@lers.ru>
2020-10-07 12:27:14 +1000

    asdmin

--- src/app/@core/models/Finiko.ts
+++ src/app/@core/models/Finiko.ts
@@ -3,4 +3,10 @@ export class Finiko {
   title: string;
   description: string;
   personalLink: string;
+  email: string;
+  techSupportLink: string;
+  telegramLink: string;
+  titleCircle: string;
+  titleCircleSub: string;
+  logoLink: string;
 }
